86cfe85186a6f245e1d5d10028a675e59650f17e
Mohamed Abdelhay <93220261+MohamedAbdelhay00@users.noreply.github.com>
2023-03-15 11:00:28 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const fetchResponse = async(chat) => {
     try{
-        const response = await fetch('https://chat-gpt-server-theta.vercel.app/', {
+        const response = await fetch('https://chat-gpt-server-bb1u.vercel.app/', {
             method: 'POST', 
             headers: {
                 "Content-Type": "application/json"
